3eef7c9834022f7d941658ec19d16ace5c7df77e
Siva-bhamidimarri <bhamidimarrisiva44@gmail.com>
2026-05-25 23:31:47 +0530

    added cards

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,1 +1,15 @@
-console.log("Hello from JavaScript");
+console.log("Hello from JS");
+var users = [
+    {"name" : "John", "gender" : "Male", "img" : "images/john.png"},
+    {"name" : "Jane", "gender" : "Female", "img" : "images/jane.png"}
+];
+var currentUserIndex = 0;
+function toggleCard(){
+    currentUserIndex =(currentUserIndex + 1) % 2;
+    var user = users[currentUserIndex];
+
+    // DOM Manipulation
+    document.getElementById("userImage").src = user.img;
+    document.getElementById("userGender").innerHTML = user.gender;
+    document.getElementById("userName").innerHTML = user.name;
+}
